ffeaaf1cc8f2179e297e1df45a3a1be5a9921fae
muiezarif <36100308+muiezarif@users.noreply.github.com>
2023-05-08 05:32:13 +0500

    first commit

--- src/examples/Navbars/DefaultNavbar/index.js
+++ src/examples/Navbars/DefaultNavbar/index.js
@@ -97,8 +97,8 @@ function DefaultNavbar({ transparent, light, action }) {
           </SoftTypography>
         </SoftBox>
         <SoftBox color="inherit" display={{ xs: "none", lg: "flex" }} m={0} p={0}>
-          <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" light={light} />
-          <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} />
+          {/* <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" light={light} /> */}
+          {/* <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} /> */}
           <DefaultNavbarLink
             icon="account_circle"
             name="sign up"
@@ -112,7 +112,7 @@ function DefaultNavbar({ transparent, light, action }) {
             light={light}
           />
         </SoftBox>
-        {action &&
+        {/* {action &&
           (action.type === "internal" ? (
             <SoftBox display={{ xs: "none", lg: "inline-block" }}>
               <SoftButton
@@ -141,7 +141,7 @@ function DefaultNavbar({ transparent, light, action }) {
                 {action.label}
               </SoftButton>
             </SoftBox>
-          ))}
+          ))} */}
         <SoftBox
           display={{ xs: "inline-block", lg: "none" }}
           lineHeight={0}
